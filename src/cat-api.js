a90326412c8a6d43d8498e02174f34c100a1cb19
DemioSan <132845079+DemioSan@users.noreply.github.com>
2023-10-15 20:35:52 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,25 @@
+import axios from 'axios';
+
 const url = 'https://api.thecatapi.com/v1';
 const api_key =
   'live_sQSyIMtkELEPBN4s4ew1WiS3cC6nMYmYFfG7F4bWD9dvUcOwd64b8voMHI7JctrG';
 
 export function fetchBreeds() {
-  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
-    if (!response.ok) {
+  return axios.get(`${url}/breeds`, { params: { api_key } }).then(response => {
+    if (response.status !== 200) {
       throw new Error(response.status);
     }
-    return response.json();
+    return response.data;
   });
 }
 
 export function fetchCatByBreed(breedId) {
-  return fetch(
-    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios
+    .get(`${url}/images/search`, { params: { api_key, breed_ids: breedId } })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    });
 }
